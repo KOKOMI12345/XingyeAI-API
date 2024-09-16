bf603d98f1303bea9171e15c48c1321886508b2a
符玄 <3072252442@qq.com>
2024-09-16 14:26:08 +0800

    Update metadata.py

--- XingyeAI/metadata.py
+++ XingyeAI/metadata.py
@@ -16,7 +16,7 @@ LOGO = r"""
       """
 AUTHOR = "符玄(KOKOMI12345)"
 VERSION = "0.1.0-alpha"
-GITHUB = "https://github.com/KOKOMI12345/XingyeAI"
+GITHUB = "https://github.com/KOKOMI12345/XingyeAI-API"
 DATETIME_NOW = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
 INFO_STRING = f"{CYAN_ASNI}{BOLD_ASNI}@Author: {AUTHOR}\n@Version: {VERSION}\n@Github: {GITHUB}\n@Datetime: {DATETIME_NOW}\n{END_ASNI}"
